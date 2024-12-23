ee077032220f3de1baf8950fb685693b0c891e60
Marty Neal <martin.neal@interprose.com>
2024-12-22 20:05:14 -0800

    days 14 part 2

--- src/main/kotlin/day14/Main.kt
+++ src/main/kotlin/day14/Main.kt
@@ -1,47 +1,57 @@
 package day14
 
 import java.lang.Math.floorMod
-import kotlin.math.max
 
-class Main {
+class Main(val ROWS: Int = 103, val COLS: Int = 101) {
     companion object {
-//        const val ROWS = 7
-//        const val COLS = 11
-        const val ROWS = 103
-        const val COLS = 101
-
         @JvmStatic
         fun main(args: Array<String>) {
-//            Main().process(
-//                """
-//                      p=0,4 v=3,-3
-//                      p=6,3 v=-1,-3
-//                      p=10,3 v=-1,2
-//                      p=2,0 v=2,-1
-//                      p=0,0 v=1,3
-//                      p=3,0 v=-2,-2
-//                      p=7,6 v=-1,-3
-//                      p=3,0 v=-1,-2
-//                      p=9,3 v=2,3
-//                      p=7,3 v=-1,2
-//                      p=2,4 v=2,-3
-//                      p=9,5 v=-3,-3""", 0
-//            ).also(::println).takeIf { it == 12 } ?: error("example failed")
+            Main(7, 11).process(
+                """
+                      p=0,4 v=3,-3
+                      p=6,3 v=-1,-3
+                      p=10,3 v=-1,2
+                      p=2,0 v=2,-1
+                      p=0,0 v=1,3
+                      p=3,0 v=-2,-2
+                      p=7,6 v=-1,-3
+                      p=3,0 v=-1,-2
+                      p=9,3 v=2,3
+                      p=7,3 v=-1,2
+                      p=2,4 v=2,-3
+                      p=9,5 v=-3,-3""", 100
+            ).also(::println).takeIf { it == 12 } ?: error("example failed")
             val input = Utils.getInput(14)
             println(Main().process(input, 100)) //225552000
-            println(Main().process2(input)) // 58 is wrong? 99 is wrong 57 is wrong 59 is wrong
+            println(Main().process2(input)) // 7371
         }
     }
 
-    fun process(input: String, seconds: Int): Int =
+    fun parseInput(input: String): List<Pair<Pair<Int, Int>, Pair<Int, Int>>> =
         input.trimIndent().lines().map { line ->
             val (p, v) = line.split(" v=", limit = 2)
             val (px, py) = p.split("p=", limit = 2)[1].split(",", limit = 2).map { it.toInt() }
             val (vx, vy) = v.split(",", limit = 2).map { it.toInt() }
-            Pair(floorMod((px + vx * seconds), COLS), floorMod((py + vy * seconds), ROWS))
+            Pair(px to py, vx to vy)
         }
-        .also(::printGrid)
-        .let(::safetyFactor)
+
+    fun process(input: String, seconds: Int): Int =
+        parseInput(input)
+            .map { (p, v) ->
+                Pair(
+                    floorMod((p.first + v.first * seconds), COLS),
+                    floorMod((p.second + v.second * seconds), ROWS)
+                )
+            }
+            .also(::printGrid)
+            .let(::safetyFactor)
+
+    fun printGrid(positions: List<Pair<Int, Int>>) {
+        val grid = Array(ROWS) { CharArray(COLS) { '.' } }
+        positions.forEach { (x, y) -> grid[y][x] = '#' }
+        println(grid.joinToString("\n") { it.joinToString("") })
+    }
+
 
     fun safetyFactor(positions: List<Pair<Int, Int>>): Int = positions
         .fold(listOf(0, 0, 0, 0)) { a, e ->
@@ -54,32 +64,22 @@ class Main {
             }
         }.fold(1) { acc, i -> acc * i }
 
-    fun printGrid(positions: List<Pair<Int, Int>>) {
-        val grid = Array(ROWS) { CharArray(COLS) { '.' } }
-        positions.forEach { (x, y) -> grid[y][x] = '#' }
-        println(grid.joinToString("\n") { it.joinToString("") })
-    }
-
-    fun parseInput(input: String): List<Pair<Pair<Int, Int>, Pair<Int, Int>>> =
-        input.trimIndent().lines().map { line ->
-            val (p, v) = line.split(" v=", limit = 2)
-            val (px, py) = p.split("p=", limit = 2)[1].split(",", limit = 2).map { it.toInt() }
-            val (vx, vy) = v.split(",", limit = 2).map { it.toInt() }
-            Pair(px to py, vx to vy)
-    }
 
     fun process2(input: String): Int {
         val parsed = parseInput(input)
         val velocities = parsed.map { it.second }
         return generateSequence(parsed.map { it.first }) { positions ->
-            positions.zip(velocities) { p, v -> floorMod(p.first + v.first, COLS) to floorMod(p.second + v.second, ROWS) }
-        }.mapIndexed { i, it -> i to safetyFactor(it) }
-            .take(max(ROWS, COLS))
-//            .onEach { println(it) }
-            .minBy { it.second }
-            .also {
-                process(input, it.first)
+            positions.zip(velocities) { p, v ->
+                floorMod(p.first + v.first, COLS) to floorMod(
+                    p.second + v.second,
+                    ROWS
+                )
             }
+        }
+            .mapIndexed { i, it -> i to safetyFactor(it) }
+            .take(ROWS * COLS)
+            .minBy { it.second }
+            .also { process(input, it.first) }
             .first
     }
 }
